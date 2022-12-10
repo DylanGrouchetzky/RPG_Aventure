ae18845dcb9e8602c409287fef2ac79080de0daf
DylanG <mrgrouchetzkydylan@gmail.com>
2022-12-10 17:59:34 +0100

    Suppression des fichier inutile

--- asset/php/traitement/degatInfliger.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$_SESSION['degat'] = [
-    'hero' => $_SESSION['monster']['atk'] - $_SESSION['hero']['def'],
-    'monster' => $_SESSION['hero']['atk'] - $_SESSION['monster']['def'],
-];
-
-
-?>
